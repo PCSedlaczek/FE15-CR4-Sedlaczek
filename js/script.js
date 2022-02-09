6d685dc789d91c39ca873f75011bf251af38cede
Petra Christina <p.sedlaczek@drei.at>
2022-02-09 13:12:24 +0100

    Implement Sort function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
+// Get tasks from JSON file
 tasks = JSON.parse(tasks);
-var result = document.getElementById("tasks");
 
-tasks.forEach(function (val, i) {
-  result.innerHTML += `
+let result = document.getElementById("tasks");
+
+// Create task cards
+function createCards() {
+  result.innerHTML = "";
+
+  tasks.forEach(function (val, i) {
+    result.innerHTML += `
     <!-- Card -->
     <div class="col px-2">
       <div class="card">
@@ -16,11 +22,11 @@ tasks.forEach(function (val, i) {
           <div class="col">
             <!-- Check button -->
             <button class="btn bg-white text-success p-0">
-              <i id="check-${i}" class="bi bi-check-circle-fill" role="img" aria-label="Checked"></i>
+              <i id="check${i}" class="bi bi-check-circle-fill" role="img" aria-label="Checked"></i>
             </button>
             <!-- Bookmark button -->
             <button class="btn bg-white p-0">
-              <i id="mark-${i}" class="bi bi-bookmark text-muted" role="img" aria-label="Bookmark"></i>
+              <i id="mark${i}" class="bi" role="img" aria-label="Bookmark"></i>
             </button>
             <!-- Drag handle -->
             <button class="btn bg-white p-0">
@@ -46,10 +52,10 @@ tasks.forEach(function (val, i) {
               Priority level:
             </b>
             <!-- Importance button -->
-            <button id="prio-${i}" class="prio btn btn-sm btn-success rounded-pill py-0 mb-1">${val.importance}</button>
+            <button id="prio${i}" class="prio btn btn-sm btn-success rounded-pill py-0 mb-1">${val.importance}</button>
           </p>
           <!-- Deadline -->
-          <p id="date-${i}" class="mb-0">
+          <p id="date${i}" class="mb-0">
             <b>
               <i class="bi bi-calendar3" aria-hidden="true"></i>
               Deadline:
@@ -65,7 +71,7 @@ tasks.forEach(function (val, i) {
               Delete
             </button>
             <!-- Done button -->
-            <button id="done-${i}" class="btn btn-sm btn-success">
+            <button id="done${i}" class="btn btn-sm btn-success">
               <i class="bi bi-check-circle" aria-hidden="true"></i>
               Done
             </button>
@@ -74,56 +80,112 @@ tasks.forEach(function (val, i) {
     </div>
     `
 
-  let marked = document.getElementById(`mark-${i}`)
-  let done = document.getElementById(`done-${i}`)
-  let check = document.getElementById(`check-${i}`)
-  let date = document.getElementById(`date-${i}`)
-  let prio = document.getElementById(`prio-${i}`)
+    // Get priority from localStorage
+    let prio = document.getElementsByClassName("prio")
+    let saved = localStorage.getItem(`prio${i}`);
 
-  // Set Bookmark button
-  if (val.marked == true) {
-    marked.classList = "bi bi-bookmark-fill text-warning"
-  } else {
-    marked.classList = "bi bi-bookmark text-muted"
-  }
+    if (saved) {
+      prio[i].innerText = saved;
+      tasks[i].importance = saved;
+    }
 
-  // Set Check button
-  if (val.done == true) {
-    check.style.visibility = "visible";
-    done.disabled = true
-  } else {
-    check.style.visibility = "hidden";
-    done.disabled = false
-  }
+    // Set button color according to priority level
+    btncolor(i)
 
-  // Show/hide deadline
-  if (val.deadline == false) {
-    date.style.visibility = "hidden"
-  } else {
-    date.style.visibility = "visible"
-  }
+    // Add EventListener to importance button
+    for (let i = 0; i < prio.length; i++) {
+      prio[i].addEventListener("click", function(){
+        increase(i);
+      });
+    }
 
-})
+    // Set Bookmark button
+    let mark = document.getElementById(`mark${i}`)
 
-function increase() {
-  let num = Number(this.innerText);
-  console.log(this)
+    if (val.mark == true) {
+      mark.classList.add("bi-bookmark-fill", "text-warning")
+    } else {
+      mark.classList.add("bi-bookmark", "text-muted")
+      mark.classList.remove("bi-bookmark-fill", "text-warning")
+    }
 
+    // Set Checkmark icon & disable Done button
+    let check = document.getElementById(`check${i}`)
+    let done = document.getElementById(`done${i}`)
+
+    if (val.done == true) {
+      check.style.visibility = "visible";
+      done.disabled = true
+    } else {
+      check.style.visibility = "hidden";
+      done.disabled = false
+    }
+
+    // Hide deadline if not set
+    let date = document.getElementById(`date${i}`)
+
+    if (val.deadline == false) {
+      date.style.visibility = "hidden"
+    } else {
+      date.style.visibility = "visible"
+    }
+  })
+}
+
+// Increase Priority level
+function increase(i) {
+  let saved = localStorage.getItem(`prio${i}`);
+  let num = tasks[i].importance
+
+    if (saved) {
+      num = saved;
+    }
+  
+  let id = document.getElementsByClassName("prio")[i].id;
+
+  // Increase priority up to 5
   if (num < 5) {
-    this.innerText = num + 1
+    num++;
+    tasks[i].importance++;
+    document.getElementsByClassName("prio")[i].innerText = num;
+    localStorage.setItem(id, num)
   }
-  if (num < 1) {
-    this.classList = "prio btn btn-sm btn-success rounded-pill py-0 mb-1"
-  } else if (num < 3) {
-    this.classList = "prio btn btn-sm btn-warning rounded-pill py-0 mb-1"
+  btncolor(i)
+}
+
+function btncolor(i) {
+  let prio = document.getElementsByClassName("prio")
+  let imp = tasks[i].importance
+
+  // Change importance color according to priority
+  if (imp < 2) {
+    prio[i].classList.add("btn-success")
+  } else if (imp < 4) {
+    prio[i].classList.add("btn-warning")
+    prio[i].classList.remove("btn-success")
   } else {
-    this.classList = "prio btn btn-sm btn-danger rounded-pill py-0 mb-1"
+    prio[i].classList.remove("btn-success", "btn-warning")
+    prio[i].classList.add("btn-danger")
   }
 }
 
-// Add EventListener to importance button
-var imp = document.getElementsByClassName("prio");
+// Sort by priority
+function prioSort() {
 
-for (let i in imp) {
-  imp[i].addEventListener("click", increase)
+  // Sort descending
+  tasks.sort(function(a, b) {
+    return b.importance - a.importance
+  })
+
+  // console.table(tasks)
+  createCards();
 }
+
+// Call createCards function
+createCards();
+
+// Add EventListener to sort button
+let sort = document.getElementById("sort")
+sort.addEventListener("click", function() {
+  prioSort();
+})
